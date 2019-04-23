c6d029abe59feb5b17a08c830debd6b254c1f5c4
Igor Babichev <himic@himic.ru>
2019-04-23 12:44:00 +0500

    fix

--- docker/mysql/create_base.sh
+++ docker/mysql/create_base.sh
@@ -6,6 +6,10 @@ mysqladmin -u root password $MYPASSWD;
 mysql -uroot -p$MYPASSWD -e "CREATE DATABASE IF NOT EXISTS domain_statistic DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
 mysql -uroot -p$MYPASSWD -e "CREATE USER 'pma'@'%' IDENTIFIED BY 'pma';"
 mysql -uroot -p$MYPASSWD -e "GRANT ALL PRIVILEGES ON * . * TO 'pma'@'%';"
+mysql -uroot -p$MYPASSWD -e "GRANT ALL PRIVILEGES ON * . * TO 'pma'@'localhost';"
+mysql -uroot -p$MYPASSWD -e "CREATE USER 'domain_statistic'@'%' IDENTIFIED BY 'domain_statistic';"
+mysql -uroot -p$MYPASSWD -e "GRANT ALL PRIVILEGES ON * . * TO 'domain_statistic'@'%';"
+mysql -uroot -p$MYPASSWD -e "GRANT ALL PRIVILEGES ON * . * TO 'domain_statistic'@'localhost';"
 mysql -uroot -p$MYPASSWD -e "FLUSH PRIVILEGES;"
 sleep 5
 mysql -uroot -p$MYPASSWD domain_statistic < /root/structure.sql
